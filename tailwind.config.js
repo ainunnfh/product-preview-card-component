3030ed27bc0187e2d8a9852d58f5fe2a7080d6e7
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-01-20 22:23:40 +0700

    🆙update (tailwind.config): config new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // sans: ["Inter", "system-ui", "Arial", "sans-serif"],
+        // mono: ["JetBrains Mono", "monospace"],
+        Montserrat: ["Montserrat", "system-ui", "Arial", "sans-serif"],
+        Fraunces: ["Fraunces", "system-ui", "Arial", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
